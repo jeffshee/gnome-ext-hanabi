8bddccfbbbe06f29676ee5aa7558c805bd29df64
jeffshee <jeffshee8969@gmail.com>
2023-08-16 12:36:18 +0900

    update: copyright header

--- src/prefs.js
+++ src/prefs.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2022 Jeff Shee (jeffshee8969@gmail.com)
+ * Copyright (C) 2023 Jeff Shee (jeffshee8969@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/roundedCornersEffect.js
+++ src/roundedCornersEffect.js
@@ -1,4 +1,5 @@
 /**
+ * Copyright (C) 2023 Jeff Shee (jeffshee8969@gmail.com)
  * Copyright (C) 2022 Alynx Zhou (alynx.zhou@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
